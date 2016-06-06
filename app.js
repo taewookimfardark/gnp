a09cc0b442935ffe1edb9268b8875e27cf875135
taewookimfardark <2012142087@yonsei.ac.kr>
2016-06-06 13:47:40 +0900

    ui router

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-var app = angular.module('gnp_app',['ui.router','ngMaterial']);
+var gnp_app = angular.module('gnp_app',['ui.router','ngMaterial']);
 
+gnp_app.config(function($stateProvider, $urlRouterProvider, $mdIconProvider)
+{
+    $urlRouterProvider.otherwise("/main");
+    
+    $stateProvider
+        .state('main',{
+            url: "main",
+            templateUrl: "src/main.html"
+        })
+        .state('matchschedule',{
+            url: "matchschedule",
+            templateUrl: "src/match_schedule.html"
+        })
+        .state('matchresult',{
+            url: "matchresult",
+            templateUrl: "src/match_result.html"
+        })
+        .state('playerresult',{
+            url: "playerresult",
+            templateUrl: "src/player_result.html"
+        });
+    
+});
