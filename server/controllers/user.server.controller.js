233624ffea4b5e949940becd4ef880210630c3f6
taewookimfardark <2012142087@yonsei.ac.kr>
2016-06-09 01:02:36 +0900

    put delete

--- server/controllers/user.server.controller.js
+++ server/controllers/user.server.controller.js
@@ -38,3 +38,33 @@ exports.userById = function(req, res, next, id)
         {_id : id}, function(err, user){ if (err) {return next(err)} else {req.user = user; next();}}
     );   
 };
+
+exports.update = function(req, res, next)
+{
+    User.findByIdAndUpdate(req.user.id, req.body, function(err, user)
+    {
+        if(err)
+        {
+            return next(err);
+        }
+        else
+        {
+            res.json(user);
+        }
+    });
+};
+
+exports.delete = function(req, res, next)
+{
+    req.user.remove(function(err)
+    {
+        if(err)
+        {
+            return next(err);
+        }
+        else
+        {
+            res.json(req.user);
+        }
+    });
+};
--- server/routes/users.server.routes.js
+++ server/routes/users.server.routes.js
@@ -3,7 +3,12 @@ var users = require('../controllers/user.server.controller');
 module.exports = function(app){
     app.route('/users').post(users.create);
     app.route('/users').get(users.list);
-    app.route('/users/:userId').get(users.read);
+    app.route('/users/:userId')
+        .get(users.read)
+        .put(users.update);
+        .delete(users.delete);
+    
     app.param('userId', users.userById);
+    
 };
 
