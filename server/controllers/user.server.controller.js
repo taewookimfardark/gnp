443bce93248952843198126f64072abca635b2b3
taewookimfardark <2012142087@yonsei.ac.kr>
2016-06-09 00:57:30 +0900

    get user by id

--- server/controllers/user.server.controller.js
+++ server/controllers/user.server.controller.js
@@ -26,3 +26,15 @@ exports.list = function(req, res, next)
         }
     });
 };
+
+exports.read = function(req, res)
+{
+    res.json(req.user);
+};
+    
+exports.userById = function(req, res, next, id)
+{
+    User.findOne(
+        {_id : id}, function(err, user){ if (err) {return next(err)} else {req.user = user; next();}}
+    );   
+};
--- server/routes/users.server.routes.js
+++ server/routes/users.server.routes.js
@@ -3,5 +3,7 @@ var users = require('../controllers/user.server.controller');
 module.exports = function(app){
     app.route('/users').post(users.create);
     app.route('/users').get(users.list);
+    app.route('/users/:userId').get(users.read);
+    app.param('userId', users.userById);
 };
 
