633267bdfb425b5aeec70d48485879e32f469d09
taewookimfardark <2012142087@yonsei.ac.kr>
2016-07-23 21:02:33 +0900

    playerrecord template setting

--- controllers/playerRecordController.js
+++ controllers/playerRecordController.js
@@ -1,12 +1,24 @@
 gnp_app.controller('playerRecordController', ["$scope", "httpRequest", function ($scope, httpRequest) {
 
+    $scope.playerrecord = []
     httpRequest.send('GET','users')
         .then(
             function(res)
             {
-                $scope.userData = res.data;
-                console.log("userdata!!");
-                console.log($scope.userData);
+                for(var index in res.data)
+                {
+                    var temp = {"Name" :'',"Backnumber":'',"Games":'',"Points":'',"Rebounds":'',"Assists":''}
+                    temp.Name = res.data[index].name;
+                    temp.Backnumber = res.data[index].backnumber;
+                    temp.Games = res.data[index].records.games;
+                    temp.Points = res.data[index].records.points;
+                    temp.Rebounds = res.data[index].records.rebounds;
+                    temp.Assists = res.data[index].records.assists;
+                    
+                    $scope.playerrecord.push(temp);
+                    
+                }
+
             },
             function(res)
             {
